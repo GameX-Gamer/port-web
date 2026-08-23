4a28766f142f98c268d85be63b4821c654ccb1eb
GameX-Gamer <sammimabegam123@gmail.com>
2026-08-23 12:22:29 +0530

    Completed the final scroll animation

--- script.js
+++ script.js
@@ -21,60 +21,68 @@ const observer = new IntersectionObserver(
     }
 );
 
-
 observer.observe(aboutSection)
-
-gsap.registerPlugin(ScrollTrigger);
 const canvas = document.getElementById("bg-canvas");
 const ctx = canvas.getContext("2d");
-const frameCount = 368;
+const frameCount =368;
 const frames = [];
-let currentFrame = 0;
-const frameObj = {frame:0};
+let currentFrame =0;
 
-for(let i=0; i <frameCount; i++) {
-    const img = new Image();
-    const frameNumber = String(i+1).padStart(5,"0");
-    img.src = `./assets/frames/toWEBP/img_${frameNumber}.webp`
+// frame loader
+
+for (let i = 0; i < frameCount; i++){
+    const img =new Image();
+    const frameNumber = String(i + 1).padStart(5, "0");
+    img.src = `./assets/frames/toWEBP/img_${frameNumber}.webp`;
     frames.push(img);
 }
+
+// canvas size
+
 function resizeCanvas() {
     canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    canvas.height =window.innerHeight;
     drawFrame(currentFrame);
 }
-window.addEventListener("resize",resizeCanvas);
-resizeCanvas();
+window.addEventListener("resize", resizeCanvas);
 
-function drawFrame(index) {
-    const img= frames[index];
-    if(!img || !img.complete) {
+// frame displayer
+
+function drawFrame(index){
+    const img = frames[index];
+    if(!img || !img.complete || !img.naturalWidth ){
         return;
     }
-    ctx.clearRect(0,0, canvas.width, canvas.height);
+    ctx.clearRect(
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    );
     const scale = Math.max(
         canvas.width / img.width,
-        canvas.height / img.height,
+        canvas.height / img.height
     );
     const width = img.width * scale;
     const height = img.height * scale;
     const x = (canvas.width - width) / 2;
     const y = (canvas.height - height) / 2;
-    ctx.drawImage(img, x, y, width, height);
+
+    ctx.drawImage( img, x, y, width, height);
 }
+
+// preloader
+
 function preloadFrames() {
     return Promise.all(
-        frames.map((img) => {
-            return new Promise((resolve) => {
+        frames.map(img => {
+            return new Promise(resolve => {
+
                 if (img.complete && img.naturalWidth) {
                     resolve();
                     return;
                 }
-                img.onload = () => {
-                    img.decode()
-                    .catch(() => {})
-                    .finally(resolve);
-                };
+                img.onload =resolve;
                 img.onerror = () => {
                     console.warn("Failed to load:", img.src);
                     resolve();
@@ -83,56 +91,51 @@ function preloadFrames() {
         })
     );
 }
+// scroll animation
 
-preloadFrames().then(() => {
-    console.log("all 368 frames loaded")
-    resizeCanvas();
-    currentFrame = 0;
-    drawFrame(0);
-    
-    gsap.to(frameObj, {
-        frame: 119,
-        duration: 4,
-        ease: "none",
-        onUpdate: () => {
-            const frame = Math.round(
-                frameObj.frame
-            );
-            if(frame !== currentFrame) {
-                currentFrame = frame;
-                drawFrame(currentFrame);
-            }
-        },
-        onComplete:() => {
-            console.log(
-                "intro complete of 120frames"
-            );
-            createScrollAnimation();
-        }
-    });
-});
 function createScrollAnimation() {
-    gsap.to(frameObj, {
-        frame: frameCount -1,
+    gsap.registerPlugin(ScrollTrigger);
+    const scrollObject = {
+        frame: 135
+    };
+
+    gsap.to(scrollObject, {
+        frame:frameCount - 1,
         ease: "none",
+
         scrollTrigger: {
-            trigger: ".main-sec",
-            start: "top top",
-            end: "+=4000",
-            scrub: 1,
-            pin: true,
-            pinSpacing: true,
-            anticipatePin: 1
-        },
-        onUpdate: () => {
-            const frame = Math.round(
-                frameObj.frame
-            );
-            if(frame !== currentFrame) {
-                currentFrame = frame;
-                drawFrame(currentFrame);
+            id:"flowerScroll",
+
+            // main-sec starter
+            trigger:".main-sec",
+            start:"top top",
+
+            // revel-sec end
+            endTrigger:".revel-sec",
+            end:"top top",
+            scrub: true,
+
+            onUpdate: () => {
+                const frame = Math.round(scrollObject.frame);
+                if (frame !== currentFrame) {
+                    currentFrame = frame;
+                    drawFrame(currentFrame);
+                }
+            },
+            // If we scroll back upward bring the flower back
+            onEnterBack: () => {
+                canvas.style.opacity ="1";
+            },
+            onLeaveBack: () => {
+                canvas.style.opacity ="1";
             }
         }
     });
     ScrollTrigger.refresh();
 }
+resizeCanvas();
+
+preloadFrames().then(() => {
+    console.log("Flower frames loaded");
+    createScrollAnimation();
+});
